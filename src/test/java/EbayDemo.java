226cdd9c5ada4c090409e059c53f3b6b6c826c0d
Hemru Chamuditha <chamudhitha123@gmail.com>
2025-01-16 12:25:52 +0530

    navigated to new tabbed window

--- src/test/java/EbayDemo.java
+++ src/test/java/EbayDemo.java
@@ -1,5 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
 
 public class EbayDemo {
 
@@ -10,9 +13,26 @@ public class EbayDemo {
 
         //Accessing the particular website through the get method
         driver.get("https://demo.automationtesting.in/Windows.html");
-        Thread.sleep(3000);
 
+        String parentWindow = driver.getWindowHandle();
+        System.out.println(parentWindow);
+        System.out.println(driver.getTitle());
 
+        //Clicking the "Open New Tabbed windows" Link
+        driver.findElement(By.xpath("//a[contains(text(), 'Open New Tabbed Windows')]")).click();
+
+        //clicking the click Button
+        driver.findElement(By.xpath("//a/button[contains(text(),'click')]")).click();
+
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for(String window : windowHandles){
+            if (!(window.contains(parentWindow))){
+                driver.switchTo().window(window);
+            }
+        }
+
+        System.out.println(driver.getTitle());
 
 
     }
