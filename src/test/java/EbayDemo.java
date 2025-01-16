6756dd1783dadcae30648317a6ca6139f62646d8
Hemru Chamuditha <chamudhitha123@gmail.com>
2025-01-16 11:52:22 +0530

    Practising navigating to new Tabbed window

--- src/test/java/EbayDemo.java
+++ src/test/java/EbayDemo.java
@@ -1,31 +1,19 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class EbayDemo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        //create() method initializes a new ChromeDriver instance, which opens a new Chrome browser window.
-        //WebDriver is an interface in Selenium that provides methods for interacting with web browsers, such as navigating to URLs, interacting with web elements, and controlling browser behavior.
         WebDriver driver = WebDriverManager.chromedriver().create();
-//        System.out.println(driver.getClass()); --> class org.openqa.selenium.chrome.ChromeDriver
         driver.manage().window().maximize();
-        driver.get("https://www.ebay.com/");
-        driver.findElement(By.xpath("//input[@id='gh-ac']")).click();
-        driver.findElement(By.xpath("//input[@id='gh-ac']")).clear();
-        driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("Iphone");
 
-        Select selectCategory = new Select(driver.findElement(By.id("gh-cat")));
-        selectCategory.selectByVisibleText("Cell Phones & Accessories");
-
-        driver.findElement(By.cssSelector("#gh-btn")).click();
-
-        driver.findElement(By.xpath("//span[text()='Apple iPhone X']")).click();
+        //Accessing the particular website through the get method
+        driver.get("https://demo.automationtesting.in/Windows.html");
         Thread.sleep(3000);
 
 
-    }
 
+
+    }
 }
