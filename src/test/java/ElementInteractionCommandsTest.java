45296c0e2bf87d216a41825a53aa967239a640d6
Hemru Chamuditha <chamudhitha123@gmail.com>
2025-01-18 12:28:21 +0530

    select multiple checkboxes and iterate list of web elements

--- src/test/java/ElementInteractionCommandsTest.java
+++ src/test/java/ElementInteractionCommandsTest.java
@@ -1,6 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ElementInteractionCommandsTest {
 
@@ -16,6 +19,13 @@ public class ElementInteractionCommandsTest {
         driver.findElement(By.cssSelector("div > button > span[class=\"gh-search-button__label\"]")).click();
 //        Thread.sleep(1000);
 
+        driver.findElement(By.xpath("//div[@class='gh-search-input__wrap']/input[@class='gh-search-input gh-tb ui-autocomplete-input']")).clear();
         driver.findElement(By.cssSelector("input[aria-label=\"Apple Watch Series SE\"]")).click();
+        driver.findElement(By.cssSelector("input[aria-label=\"Apple Watch Series 7\"]")).click();
+
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='s-item__title']/span[@role='heading']"));
+        for (WebElement element : elements) {
+            System.out.println(element.getText());
+        }
     }
 }
