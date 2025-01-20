c63082dc7a296897c4fffb8c9a0159b9c6ace0ed
Hemru Chamuditha <chamudhitha123@gmail.com>
2025-01-20 22:58:15 +0530

    created alertWithOk method

--- src/test/java/AlertCommandsTest.java
+++ src/test/java/AlertCommandsTest.java
@@ -1,6 +1,28 @@
-public class AlertCommandsTest {
-    public static void main(String[] args) {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-        
+public class AlertCommandsTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        alertWithOk();
+    }
+
+    private static void alertWithOk() throws InterruptedException {
+
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/alerts");
+        driver.findElement(By.cssSelector("[id='alertButton']")).click();
+
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        Thread.sleep(2000);
+        alert.accept();
+
+
     }
 }
