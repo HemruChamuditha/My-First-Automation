6555ece572eed7400b0791dafc19b590f3ef40ee
Hemru Chamuditha <chamudhitha123@gmail.com>
2025-01-20 23:39:10 +0530

    created alertWithOkAndCancel method

--- src/test/java/AlertCommandsTest.java
+++ src/test/java/AlertCommandsTest.java
@@ -4,10 +4,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+
 public class AlertCommandsTest {
     public static void main(String[] args) throws InterruptedException {
 
-        alertWithOk();
+//        alertWithOk();
+        alertWithOkAndCancel();
+
+    }
+
+    private static void alertWithOkAndCancel() throws InterruptedException {
+
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+
+        driver.get("https://demo.automationtesting.in/Alerts.html");
+        driver.findElement(By.xpath("//a[text()='Alert with OK & Cancel ']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.className("btn-primary")).click();
+        Thread.sleep(2000);
+
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
+        WebElement element = driver.findElement(By.id("demo"));
+        System.out.println(element.getText());
+        Thread.sleep(2000);
+
+
     }
 
     private static void alertWithOk() throws InterruptedException {
@@ -22,7 +47,5 @@ public class AlertCommandsTest {
         System.out.println(alert.getText());
         Thread.sleep(2000);
         alert.accept();
-
-
     }
 }
